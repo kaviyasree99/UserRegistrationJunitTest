877eee747044597c01088a334a387a06c579e9d4
kaviyasree99 <kaviyarajalingam@gmail.com>
2021-08-06 15:29:27 +0530

    Validation of phone number

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -14,6 +14,18 @@ public class UserRegistration {
     public boolean validateLastName(String LastName) {
         //Here Declaring The Name Pattern
         String Name_pattern = "^[A-Z]{1}[a-z]{2,}$";//The First Letter With Cap And Min 3 Letters
-        return pattern.matches(Name_pattern , LastName);//Matching The Name Pattern And FirstName And Returning It.
+        return pattern.matches(Name_pattern , LastName);//Matching0 The Name Pattern And LastName And Returning It.
+    }
+    //Declaring Valid Email Method
+    public boolean validateemail(String Email) {
+        //Here Declaring The Email Pattern
+        String Email_pattern = "[a-z][a-zA-Z0-9]+([._+#-][a-zA-Z0-9]+)*@[a-zA-Z]{5}+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        return pattern.matches(Email_pattern,Email);//Matching The Email Pattern And Email And Returning It.
+    }
+    //Declaring Valid Phone Number Method
+    public boolean validatePhoneNumber(String PhoneNumber) {
+        //Here Declaring The Phone Number Pattern
+        String Number_pattern = "^[9][1][ ][6-9]{1}[0-9]{9}$";
+        return pattern.matches(Number_pattern, PhoneNumber);//Matching The Phone Number Pattern And Phone Number And Returning It
     }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 public class UserRegistrationTest {
     //Defining User Registration
     UserRegistration userRegistration = new UserRegistration();
-
     //Test To Valid FirstName Starts With Cap Letter And It Has Min 3 Letters
     @Test
     //Validation Of First Name And Return True
@@ -30,7 +29,7 @@ public class UserRegistrationTest {
     @Test
     public void givenLastName_Return_True() {
         //Checking Validate LastName With Pattern And Storing In result
-        userRegistration.validateLastName("Reddy");
+        userRegistration.validateLastName("Sree");
         boolean result = userRegistration.validateFirstName("Sree");
         //Asserting The Result As Boolean Type True Or False
         Assertions.assertEquals(true, result);
@@ -41,5 +40,55 @@ public class UserRegistrationTest {
         boolean result = userRegistration.validateLastName("sree");
         //Asserting The Result As Boolean Type True Or False
         Assertions.assertEquals(false, result);
+    }
+    //UC3
+    //Test To Valid Email Starts With Small Letter
+    @Test
+    public void givenEmail_StartsWithSmallLetter_ReturnTrue() {
+        //Checking Validate Email With Pattern And Storing In result
+        boolean result = userRegistration.validateemail("kaviyarajalingam@gmail.com");
+        //Asserting The Result As Boolean Type True Or False
+        Assertions.assertEquals(true, result);
+    }
+    //Test To Valid Email Starts With Cap Letter
+    @Test
+    public void givenEmail_StartsWithCapLetter_ReturnFalse() {
+        //Checking Validate Email With Pattern And Storing In result
+        boolean result = userRegistration.validateemail("Kaviyarajalingam@gmail.com");
+        //Asserting The Result As Boolean Type True Or False
+        Assertions.assertEquals(false, result);
+    }
+    //Test To Valid Email Ends With .Com
+    @Test
+    public void givenEmail_EndsWithCom_ReturnTrue() {
+        //Checking Validate Email With Pattern And Storing In result
+        boolean result = userRegistration.validateemail("kaviyarajalingam@gmail.com");
+        //Asserting The Result As Boolean Type True Or False
+        Assertions.assertEquals(true, result);
+    }
+    //UC4
+    //Test To Phone Number Start With Country Code
+    @Test
+    public void givenPhoneNumber_StartWithCountryCode_ReturnTrue() {
+        //Checking Validate Phone Number With Pattern And Storing In result
+        boolean result = userRegistration.validatePhoneNumber("91 9876543219");
+        //Asserting The Result As Boolean Type True Or False
+        Assertions.assertEquals(true, result);
+    }
+    //Test To Phone Number Does Not Have 10Numbers Return False
+    @Test
+    public void givenPhoneNumber_DoesNotHave10Numbers_ReturnFalse() {
+        //Checking Validate Phone Number With Pattern And Storing In result
+        boolean result = userRegistration.validatePhoneNumber("91 987654321");
+        //Asserting The Result As Boolean Type True Or False
+        Assertions.assertEquals(false, result);
+    }
+    //Test To Phone Number Does Have 10Numbers Return True
+    @Test
+    public void givenPhoneNumber_DoesHave10Numbers_ReturnTrue() {
+        //Checking Validate Phone Number With Pattern And Storing In result
+        boolean result = userRegistration.validatePhoneNumber("91 9876543219");
+        //Asserting The Result As Boolean Type True Or False
+        Assertions.assertEquals(true, result);
     }
 }
