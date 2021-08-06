be489b8390e3a2e76e19810e5813076eee2285ec
kaviyasree99 <kaviyarajalingam@gmail.com>
2021-08-06 12:40:17 +0530

    Validation of Last name

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,14 +1,19 @@
+import java.util.regex.Pattern;
 /*
 Program Of UserRegistration By Using Junit Testing
  */
-import java.util.regex.Pattern;
-
 public class UserRegistration {
     private Pattern pattern;//Declaring The Pattern variable as private
-    //
+    //Declaring Validate First Name Method
     public boolean validateFirstName(String firstName) {
         //Here Declaring The Name Pattern
         String Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";//The First Letter With Cap And Min 3 Letters
-        return pattern.matches(Name_Pattern, firstName);//Matching The Name Pattern And FirstName And Returning It.
+        return pattern.matches(Name_Pattern, firstName);//Matching The Name Pattern And FirstName And Returning It
+    }
+    //Declaring Last Name Method
+    public boolean validateLastName(String LastName) {
+        //Here Declaring The Name Pattern
+        String Name_pattern = "^[A-Z]{1}[a-z]{2,}$";//The First Letter With Cap And Min 3 Letters
+        return pattern.matches(Name_pattern , LastName);//Matching The Name Pattern And FirstName And Returning It.
     }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -2,24 +2,44 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 //Declaring User Registration Test class
+
 public class UserRegistrationTest {
     //Defining User Registration
     UserRegistration userRegistration = new UserRegistration();
+
     //Test To Valid FirstName Starts With Cap Letter And It Has Min 3 Letters
     @Test
     //Validation Of First Name And Return True
     public void givenFirstName_Return_True() {
         //Checking Validate FirstName With Pattern And Storing In result
-        boolean result= userRegistration.validateFirstName("Kaviya");
+        boolean result = userRegistration.validateFirstName("Kaviya");
         //Asserting The Result As Boolean Type True Or False
-        Assertions.assertEquals(true,result);
+        Assertions.assertEquals(true, result);
     }
+
     //Test To Valid FirstName If It Starts With Small Letter Print False
     @Test
-    public void givenFirstName_Return_False(){
+    public void givenFirstName_Return_False() {
         //Checking Validate FirstName With Pattern And Storing In result
-        boolean result =userRegistration.validateFirstName("kaviya");
+        boolean result = userRegistration.validateFirstName("kaviya");
         //Asserting The Result As Boolean Type True Or False
-        Assertions.assertEquals(false,result);
+        Assertions.assertEquals(false, result);
+    }
+    //UC2
+    //Test To Valid LastName Starts With Cap Letter And It Has Min 3 Letters
+    @Test
+    public void givenLastName_Return_True() {
+        //Checking Validate LastName With Pattern And Storing In result
+        userRegistration.validateLastName("Reddy");
+        boolean result = userRegistration.validateFirstName("Sree");
+        //Asserting The Result As Boolean Type True Or False
+        Assertions.assertEquals(true, result);
+    }
+    @Test
+    public void givenLastName_Return_False() {
+        //Checking Validate LastName With Pattern And Storing In result
+        boolean result = userRegistration.validateLastName("sree");
+        //Asserting The Result As Boolean Type True Or False
+        Assertions.assertEquals(false, result);
     }
 }
